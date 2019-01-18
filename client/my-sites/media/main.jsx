c9a6549f7cb90565ba3a4e3eb9cb13cd8fe4b518
Jarda Snajdr <jsnajdr@gmail.com>
2019-01-18 14:54:05 +0100

    Media Modal: move custom styling of 'Delete' button to standard borderless scary Button

--- client/my-sites/media/main.jsx
+++ client/my-sites/media/main.jsx
@@ -167,7 +167,7 @@ class Media extends Component {
 		return [
 			{
 				action: 'delete',
-				additionalClassNames: 'media__modal-delete-item-button is-link',
+				additionalClassNames: 'is-borderless is-scary',
 				label: translate( 'Delete' ),
 				isPrimary: false,
 				disabled: false,
